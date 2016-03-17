0c6e34a1bf36bd9ad66427b93fc97577e54b75d1
Hugo <hugo.hornquist@gmail.com>
2016-03-17 23:24:06 +0100

    minor changes

--- canvas.js
+++ canvas.js
@@ -3,14 +3,9 @@ var canvas;
 window.onload = function() {
 	var cvs = document.getElementById('myCanvas')
 	canvas = cvs.getContext("2d");
-	//console.log( canvas );
-
-	//rects[0] = new Rectangle( 10, 10, "#FF0000", ctx );
-	//rects[0].redraw();
 }
 
 function drawRectangle( rectangle ) {
-	//console.log( canvas );
 	canvas.fillStyle = rectangle.color;
 	canvas.fillRect( rectangle.x, rectangle.y, rectangle.width, rectangle.height );
 }
@@ -20,13 +15,7 @@ function removeRectangle( rectangle ) {
 
 
 document.addEventListener("keypress", function registerKeypress( e ) {
-	// e.keyCode
-	// 106
-	// 107
 	console.log( e.keyCode );
-	//var x = rects[0].x;
-	//var y = rects[0].y;
-	//var paddle = rects[Object.keys(rects)[0]];
 	var paddle = rects[ownId];
 	var x = paddle.x;
 	var y = paddle.y;
@@ -45,6 +34,8 @@ document.addEventListener("keypress", function registerKeypress( e ) {
 			x += 3;
 			break;
 	}
+	// informs the server that this move is to be made,
+	// this in turns infroms all the clients that this move should be carried out 
 	sendMessage( JSON.stringify({
 		"type": "paddle-move",
 		"data": {
---
+++ game/paddle.js
@@ -0,0 +1,11 @@
+console.log("paddle.js");
+var rects = {};
+
+function Paddle( json ) {
+	this.x      = json.x;
+	this.y      = json.y;
+	this.width  = json.width;
+	this.height = json.height;
+	this.color  = json.color;
+	this.id     = json.id;
+}
